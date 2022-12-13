4420257185eb039281ba3ec498f72443afabd98d
HuddyP <hudsonrp11@gmail.com>
2022-12-13 13:33:47 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Submission.js

--- src/Submission.js
+++ src/Submission.js
@@ -1,71 +1,106 @@
 import './App.css';
-import { useState } from 'react';
+import React from 'react';
+import { Form, Input, Button } from 'antd';
 
-//To start JSON server, **CD INTO SRC FOLDER FIRST** and use command "npx json-server --watch data.json --port 3000" 
+//CURRENT ISSUE: 
 
-//imports needed: react, react-router-dom, reactDOM, json-server
+//imports needed: react, react-router-dom, reactDOM, json-server, antd
 
-export function Submit() {
+//to make json entries show on page, cd into src and run this command: npx json-server --watch data.json --port 3000
+
+const fs = require('fs');
+const FormItem = Form.Item;
+
+class Submit extends React.Component {
+
+  constructor(prop){
+    super(prop);
+    this.state = {
+      name: '',
+      email: '',
+      project: '',
+      time: ''
+    };
+  }
   
-  
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [project, setProject] = useState("");
-  const [time, setTime] = useState("");
-  const [isPending, setIsPending] = useState(false);
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
+  writeToFile = () => {
+    console.log('writeToFile called');
+    console.log('fs:', fs);
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const request = { name, email, project, time};
+      let jsonData = JSON.parse(data);
 
-    console.log(request);
+      jsonData.push(this.state);
 
-    setIsPending(true);
-
-      fetch("http://localhost:3000/request", {
-        method: "POST",
-        headers: { "Content-Type": "applicaton/json" },
-        body: JSON.stringify(request)
-      }).then(() => {
-        console.log("New Request Submitted!");
+      fs.writeFile('data.json', JSON.stringify(jsonData), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('Data written to file');
       });
-  };
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.writeToFile();
+    this.setState({ message: 'Your request has been received by the zookeeper and will be given to the monkeys soon' });
+  }
   
+  render() {
     return (
         <div>
             <h1>Submit your request for our code monkeys: </h1>
-            <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={name}
-              placeholder="Name"
-              onChange={(e) => setName(e.target.value)}
+            <Form>
+            <FormItem>
+              <Input
+              name="name"
+              value={this.state.name}
+              placeholder="Full Name"
+              onChange={this.handleChange}
             />
-            <input
-              type="text"
-              value={email}
+            </FormItem>
+            <FormItem>
+            <Input
+              name="email"
+              value={this.state.email}
               placeholder="Email Address"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={this.handleChange}
             />
-            <input
-              type="text"
-              value={project}
+            </FormItem>
+            <FormItem>
+            <Input
+              name="project"
+              value={this.state.project}
               placeholder="What project are you requesting?"
-              onChange={(e) => setProject(e.target.value)}
+              onChange={this.handleChange}
             />
-            <input
-              type="text"
-              value={time}
+            </FormItem>
+            <FormItem>
+            <Input
+              name="time"
+              value={this.state.time}
               placeholder="When do you need the project done?"
-              onChange={(e) => setTime(e.target.value)}
+              onChange={this.handleChange}
             />
-            <button type="Submit">Submit Request</button>
-            { isPending && <div className="Message"><p>Your request has been received by the zookeeper and will be given to the monkeys soon</p></div> }
-            </form>
+            </FormItem>
+            <Button type={this.props.type} onClick={this.handleSubmit}>Submit Request</Button>
+            </Form>
             
         </div>
     ); 
+  }
 }
 
 export default Submit;
